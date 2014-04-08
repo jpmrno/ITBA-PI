d751875d6b20fca74eb88962429c767af0560d60
Juan <jpmrno@gmail.com>
2014-04-08 10:52:38 -0300

    Create EJ8.c

---
+++ TP06/EJ8.c
@@ -0,0 +1,21 @@
+//
+//  Ej 8.c
+//  Programacion Imperativa
+//
+//  Created by Juan Moreno on 3/10/14.
+//  Copyright (c) 2014 Juan Moreno. All rights reserved.
+//
+
+/* Ejercicio 8
+
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	
+}
+
+int floor() {
+	
+}
