cc9cffd12c8118e0f8d766147d9a303f5765c21b
Juan <jpmrno@gmail.com>
2014-04-08 11:13:11 -0300

    Update EJ8.c

--- TP06/EJ8.c
+++ TP06/EJ8.c
@@ -11,11 +11,22 @@
 */
 
 #include <stdio.h>
+#include "getnum.h"
+
+double redondeo(double numero, int decimales);
 
 int main(void) {
-	
+	double numero, redondeado;
+	int decimales;
+
+	numero = getint("Numero a redondear?: ");
+	decimales = getint("Cantidad de decimales a redondear?: ");
+
+	redondeado = floor(numero, decimales);
 }
 
-int floor() {
+double redondeo(double numero, int decimales) {
 	
+	
+	return numero;
 }
